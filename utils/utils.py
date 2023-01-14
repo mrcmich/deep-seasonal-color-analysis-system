e90af317dbfb0bd05564d5d6113d5c8fc604b126
Francesco Baraldi <62619792+francescobaraldi@users.noreply.github.com>
2023-01-14 15:41:30 +0100

    Update utils file
    Added function to parse arguments from command line

--- utils/utils.py
+++ utils/utils.py
@@ -1,4 +1,5 @@
 import torch
+import argparse
 
 # Converts image (torch.Tensor instance) from (H, W, D) to (D, H, W) by swapping its axes.
 def from_HWD_to_DHW(img_HWD):
@@ -15,3 +16,10 @@ def from_key_to_index(dictionary, key):
 # Counts the number of learnable parameters of model (parameters whose attribute requires_grad set to True).
 def count_learnable_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+def parse_arguments_train_pipeline():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--evaluate', type=bool, default=False, help='If True, validation is used', metavar='')
+    parser.add_argument('--n_epochs', type=int, default=50, help='Number of epochs in the validation case', metavar='')
+    parser.add_argument('--learning_rate', type=float, default=0.01, help='Learning rate', metavar='')
+    return parser.parse_args()
