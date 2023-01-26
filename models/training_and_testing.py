697d795c2ae8d0414549aa26fbe2e5f0b9d855b6
Matteo Pagliani <64635883+MatteoPagliani@users.noreply.github.com>
2023-01-26 10:40:14 +0100

    Created function for hpo

--- models/training_and_testing.py
+++ models/training_and_testing.py
@@ -6,6 +6,11 @@ import signal
 import sys
 import time
 import math
+from functools import partial
+from ray import tune
+from ray.air import session
+from ray.air.checkpoint import Checkpoint
+import os
 
 
 # A single epoch of training of model on data_loader. If training is set to False, validation/testing is carried out instead, 
@@ -152,10 +157,144 @@ def train_model(
     
         return training_results
     
-    except (InterruptSignalError, OSError) as e:
-        print(e)
+    except (InterruptSignalError, OSError):
         model_on_device.eval()
         return training_results
+
+
+# TODO: take out class InterruptSignalError and function handler from function train_model above
+# and keep the ones below (defined out of the function)
+
+
+class InterruptSignalError(Exception):
+        pass
+
+def handler(signum, frame, evaluate, clock_start):
+    '''
+    Function to catch stop signals which can occur when training in SLURM environment,
+    in order to save the progress done so far (either saving the weights or the training results),
+    avoiding terminating the training losing all the work.
+    '''
+    print(f"\nSignal handler got signal {signum}")
+    print(f"Training interrupted after around {math.ceil((time.time() - clock_start) / 60)} minutes")
+    if evaluate:
+        print("Returning the training results obtained since now")
+    else:
+        print("Saving model weights trained since now")
+    raise InterruptSignalError()
+
+
+# Function for performing hpo of model on dataset. If evaluate is set to True, the dataset is split 85/15 into a training set
+# and a validation set, otherwise the entire dataset is used for training. If verbose is set to True, additional info is
+# printed to console during training.
+# ---
+# config: dict with hyperparameters search space
+# device: device on which to load data and model ('cpu' for cpu).
+# dataset: instance of class extending torch.utils.data.Dataset.
+# score_fn: function to be used to evaluate a batch of predictions against the corresponding batch of targets.
+# num_workers: integer or tuple representing the number of workers to use when loading train and validation data.
+# from_checkpoint: bool that establishes whether to resume a previous hpo run from the last checkpoint saved
+def hpo(config, device, model, dataset, n_epochs, score_fn, loss_fn, num_workers=(0, 0), evaluate=False, verbose=False):
+    
+    model_on_device = model.to(device)
+
+    # parameters to tune
+    optimizer = torch.optim.Adam(model_on_device.parameters(), lr=config["lr"])
+    lr_scheduler = torch.optim.lr_scheduler.LinearLR(optimizer, start_factor=config["start_factor"])
+    batch_size = config["batch_size"]
+
+    # restore a checkpoint
+    if config["from_checkpoint"]:
+        loaded_checkpoint = session.get_checkpoint()
+        if loaded_checkpoint:
+            with loaded_checkpoint.as_directory() as loaded_checkpoint_dir:
+                model_state, optimizer_state = torch.load(os.path.join(loaded_checkpoint_dir, "checkpoint.pt"))
+            model_on_device.load_state_dict(model_state)
+            optimizer.load_state_dict(optimizer_state)
+
+    num_workers_train = 0
+    num_workers_test = 0
+    if type(num_workers) is tuple:
+        num_workers_train = num_workers[0]
+        num_workers_test = num_workers[1]
+    else:
+        num_workers_train = num_workers_test = num_workers
+
+    if evaluate:
+        n_train_samples = round(0.85 * len(dataset))
+        n_val_samples = len(dataset) - n_train_samples
+        dataset_train, dataset_val = random_split(
+            dataset, lengths=[n_train_samples, n_val_samples], generator=torch.Generator().manual_seed(99))
+        dl_train = DataLoader(dataset_train, batch_size=batch_size, shuffle=True, drop_last=True, num_workers=num_workers_train)
+        dl_val = DataLoader(dataset_val, batch_size=batch_size, num_workers=num_workers_test)
+    else:
+        dl_train = DataLoader(dataset, batch_size=batch_size, shuffle=True, drop_last=True, num_workers=num_workers_train)
+        dl_val = None
+
+    training_results = {
+        'average_train_loss': [],
+        'average_val_loss': [],
+        'average_train_score': [],
+        'average_val_score': []
+    }
+
+    print(f'Device: {device}.')
+
+    clock_start = time.time()
+    
+    if "win" not in sys.platform:
+        signal.signal(signal.SIGUSR1, partial(handler, evaluate=evaluate, clock_start=clock_start))
+        signal.signal(signal.SIGTERM, partial(handler, evaluate=evaluate, clock_start=clock_start))
+        signal.signal(signal.SIGINT, partial(handler, evaluate=evaluate, clock_start=clock_start))
+
+    try:
+        for epoch in range(n_epochs):
+            model_on_device.train()
+
+            average_train_loss, average_train_score = training_or_testing_epoch_(
+                device, model_on_device, dl_train, score_fn, loss_fn, training=True, optimizer=optimizer)
+            average_train_score = average_train_score.mean().item()
+            training_results['average_train_loss'].append(average_train_loss)
+            training_results['average_train_score'].append(average_train_score)
+
+            if verbose is True:
+                print(f'--- Epoch {epoch + 1}/{n_epochs} ---')
+                print(f'average_train_loss: {average_train_loss}, average_train_score: {average_train_score}')
+
+            if dl_val is None:
+                continue
+            
+            model_on_device.eval()
+
+            with torch.no_grad():
+                average_val_loss, average_val_score = training_or_testing_epoch_(device, model_on_device, dl_val, score_fn, loss_fn)
+
+            average_val_score = average_val_score.mean().item()
+            training_results['average_val_loss'].append(average_val_loss)
+            training_results['average_val_score'].append(average_val_score)
+
+            # save a checkpoint
+            os.makedirs("FastSCNN", exist_ok=True)
+            torch.save(
+                (model_on_device.state_dict(), optimizer.state_dict()), "FastSCNN/checkpoint.pt")
+            checkpoint = Checkpoint.from_directory("FastSCNN")
+            session.report({"loss": average_val_loss, "score": average_val_score}, checkpoint=checkpoint)  # TODO: keywords must be strings
+
+            if verbose is True:
+                print(f'average_val_loss: {average_val_loss}, average_val_score: {average_val_score}')
+
+            if lr_scheduler is not None:
+                lr_scheduler.step()
+
+        clock_end = time.time()
+
+        model_on_device.eval()
+
+        print(f'\nTraining completed in around {math.ceil((clock_end - clock_start) / 60)} minutes.')
+    
+    except (InterruptSignalError, OSError):
+        model_on_device.eval()
+
 
 # Function for testing model on dataset. Returns the average score along dataset's batches.
 # ---
