917d7ba183bdb8470e5aa8a5808cfedd59d62f82
Francesco Baraldi <francesco_baraldi@icloud.com>
2023-02-01 16:25:07 +0100

    updated slurm scripts

--- slurm_scripts/training_demo/models_training_Deeplabv3_demo.sh
+++ slurm_scripts/training_demo/models_training_Deeplabv3_demo.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #SBATCH --partition=students-prod
-#SBATCH --gres=gpu:1
+#SBATCH --gres=gpu:2
+#SBATCH --gpus=2
 #SBATCH --error=models/outputs/models_training_Deeplabv3_demo.err
 #SBATCH --output=models/outputs/models_training_Deeplabv3_demo.out
 
-python models_training_Deeplabv3_demo.py --evaluate=False --n_epochs=30
+python models_training_demo.py --model_name=deeplab --evaluate=False --n_epochs=30
