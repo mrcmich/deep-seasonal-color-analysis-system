bdecb5459efe29fc2a7290a76a5c3bbe4d248e80
Francesco Baraldi <francesco_baraldi@icloud.com>
2023-02-01 08:33:37 +0100

    modified file for training Deeplabv3 to use only ray tune

--- models_training_Deeplabv3_demo.py
+++ models_training_Deeplabv3_demo.py
@@ -34,27 +34,27 @@ if __name__ == "__main__":
     test_dataset = dataset.MyDataset(X_test, Y_test, image_transform, target_transform)
 
     # training hyperparameters
-    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    # if possible, exploit multiple GPUs
     batch_size = 32
     n_epochs = args.n_epochs if args.evaluate else (args.n_epochs // 2)
 
     # model, loss, score function
-    model_name = 'deeplabv3_ccncsa'
     model = deeplabv3.deeplabv3_resnet50(num_classes=n_classes)
     loss_fn = nn.CrossEntropyLoss()
     score_fn = metrics.batch_mIoU
+    
+    device = "cpu"
+    if torch.cuda.is_available():
+        device = "cuda:0"
+        if torch.cuda.device_count() > 1:
+            model = nn.DataParallel(model)
 
     # optimizer
     learning_rate = args.learning_rate
-    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    optimizer = torch.optim.Adam
 
     model_summary = torchsummary.summary(model, input_data=(batch_size, 3, tH, tW), batch_dim=None, verbose=0)
     print(model_summary)
-    
-    # training
-    results = training_and_testing.train_model(
-        device, model, train_dataset, batch_size, n_epochs, score_fn, loss_fn, optimizer, 
-        lr_scheduler=None, evaluate=args.evalute, verbose=True)
     
     # model parameters
     cfg = {
@@ -68,7 +68,7 @@ if __name__ == "__main__":
     cpus_per_trial = 0
     gpus_per_trial = torch.cuda.device_count()
     num_samples = 1  # Number of times each combination is sampled (n_epochs are done per sample)
-    evaluate = False
+    evaluate = args.evaluate
     if evaluate:
         metrics_columns = ["train_loss", "train_score", "val_loss", "val_score", "training_iteration"]
     else:
@@ -79,7 +79,7 @@ if __name__ == "__main__":
 
     results = tune.run(partial(training_and_testing.train_model_with_ray,
         device=device, model=model, dataset=train_dataset, n_epochs=n_epochs, score_fn=score_fn, loss_fn=loss_fn, 
-        optimizer=optimizer, lr_scheduler=None, num_workers=(0,0), evaluate=args.evalute),
+        optimizer=optimizer, lr_scheduler=None, num_workers=(0,0), evaluate=evaluate),
         config=cfg,
         num_samples=num_samples,
         resources_per_trial={"cpu": cpus_per_trial, "gpu": gpus_per_trial},
